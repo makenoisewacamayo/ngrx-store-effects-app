848e9502d961f51dc08a2b889f4c5f508515a171
Rafael Avaria G <rafael.avaria@gmail.com>
2018-02-13 22:13:22 -0300

    Add router actions and effects and finish to update an delete a pizza

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators';
 import * as fromStore from '../../store';
 
 import { Pizza } from '../../models/pizza.model';
@@ -36,7 +37,16 @@ export class ProductItemComponent implements OnInit {
   constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.pizza$ = this.store.select(fromStore.getSelectedPizza);
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza).pipe(
+      tap( (pizza: Pizza = null) => {
+        const pizzaExists = !!(pizza && pizza.toppings);
+        const toppings = pizzaExists
+          ? pizza.toppings.map(toppings => toppings.id)
+          : [];
+        this.store.dispatch(new fromStore.VisualiseToppings(toppings));
+      })
+    );
+
     this.toppings$ = this.store.select(fromStore.getAllToppings);
     this.visualise$ = this.store.select(fromStore.getPizzaVisualised);
   }
@@ -46,15 +56,17 @@ export class ProductItemComponent implements OnInit {
   }
 
   onCreate(event: Pizza) {
+     this.store.dispatch(new fromStore.CreatePizza(event));
   }
 
   onUpdate(event: Pizza) {
+    this.store.dispatch( new fromStore.UpdatePizza(event));
   }
 
   onRemove(event: Pizza) {
     const remove = window.confirm('Are you sure?');
     if (remove) {
-
+      this.store.dispatch(new fromStore.RemovePizza(event));
     }
   }
 }
